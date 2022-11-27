5c7e9b63a1f42169f1a6aa536c9b3efca387b029
NazarethNardone <118315259+NazarethNardone@users.noreply.github.com>
2022-11-26 21:17:13 -0300

    Add files via upload

---
+++ Proyecto10.js
@@ -0,0 +1,54 @@
+let nombre = "Programación" + " Fácil." ;
+let saludo = "Les doy la bienvenida a ";
+let id = "Pablo"
+let fraseCompleta = saludo + id + " a " +  nombre ;
+console.log(fraseCompleta);
+
+
+let frase = "JavaScript " + "es un lenguaje" + " esencial para crear" + " sitios web poderosos.";
+console.log(frase);
+
+let numeroString1 = "10";
+let numeroString2 = "20";
+let numeroString3 = "30";
+let concatenaNumeros = numeroString1 + numeroString2 + numeroString3;
+console.log(concatenaNumeros);
+
+
+
+var age= prompt("¿Cual es tu edad?");
+console.log (age)
+
+
+let n = " Soy facundo y tengo 10 años"
+console.warn (n)
+let f = " Soy fer y tengo 10 años"
+console.warn (f)
+let c = " Soy camila y tengo 10 años"
+console.warn (n)
+
+
+
+let edad;
+edad = prompt("Introduce tu edad");
+if (edad >= 18){
+alert("Puedes entrar, eres mayor de edad.");
+} else {
+alert("No puedes entrar, eres menor de edad.");
+} 
+
+let sexo;
+sexo = prompt ("Introduce tu sexo");
+if (sexo = "mujer"){
+if (sexo = "hombre")
+alert ("puedes ingresar")
+} else {
+    alert("No puedes entrar");
+} 
+
+
+
+
+
+
+
